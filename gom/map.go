37f999b99c75c80f2d6e3b6df0685ccc2dde3b6f
PhilipGuo <Philip.guo1113@gmail.com>
2018-06-04 21:32:19 +0800

    ''

--- gom/map.go
+++ gom/map.go
@@ -42,3 +42,42 @@ func (m *gom) Add(k interface{}, v interface{}, replace bool) (bool, error) {
 	}
 	return true, nil
 }
+
+//
+// remove by key
+//
+func (m *gom) RemoveByKey(k interface{}) (bool, error) {
+	if nil == m || nil == m.m {
+		return false, errors.New("m is nil")
+	}
+
+	if _, ok := m.m[k]; ok {
+		delete(m.m, k)
+		return true, nil
+	}
+	return false, errors.New("k not exists")
+}
+
+//
+// remove by value
+//
+func (m *gom) RemoveByValue(v *interface{}) (bool, error) {
+	if nil == m || nil == m.m {
+		return false, errors.New("m is nil")
+	}
+	opted := false
+	for key, val := range m.m {
+		if *val == *v {
+			delete(m.m, key)
+			opted = true
+		}
+	}
+	if opted {
+		return true, nil
+	}
+	return false, errors.New("v not exists")
+}
+
+func (m *gom) GetValueByKey(k interface{}) (bool, error) {
+	return false, nil
+}
