ffbe034bbd6da1cba1492d05055dbb196b4ebcc6
PhilipGuo <philip.guo1113@gmail.com>
2018-06-05 11:11:29 +0800

    ''

--- gom/map.go
+++ gom/map.go
@@ -1,9 +1,14 @@
 package gom
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
 type gom struct {
 	m map[interface{}]*interface{} // map
+
+	sync.RWMutex
 }
 
 const (
@@ -78,6 +83,47 @@ func (m *gom) RemoveByValue(v *interface{}) (bool, error) {
 	return false, errors.New("v not exists")
 }
 
-func (m *gom) GetValueByKey(k interface{}) (bool, error) {
-	return false, nil
+//
+// get value by key
+//
+func (m *gom) GetValueByKey(k interface{}) (interface{}, error) {
+	if nil == m || nil == m.m {
+		return nil, errors.New("m is nil")
+	}
+
+	if val, ok := m.m[k]; ok {
+		return val, nil
+	}
+	return nil, errors.New("k not exists")
+}
+
+//
+// get keys([]interface{}) by value
+//
+func (m *gom) GetKeysByValue(v *interface{}) ([]interface{}, error) {
+	if nil == m || nil == m.m {
+		return nil, errors.New("m is nil")
+	}
+
+	var keys []interface{}
+	for key, val := range m.m {
+		if *val == *v {
+			keys = append(keys, key)
+		}
+	}
+
+	return keys, nil
+}
+
+//
+// get count
+//
+func (m *gom) GetCount() int {
+	if nil == m || nil == m.m {
+		return 0
+	}
+
+	m.Lock()
+	defer m.Unlock()
+	return len(m.m)
 }
